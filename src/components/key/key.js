fa7046b4f10fa42148bc838b6e6669a5c69b6b7a
bendevman <bendevman@gmail.com>
2023-05-01 12:11:54 +0300

    refactor: change identefier classes to data atrr

--- src/components/key/key.js
+++ src/components/key/key.js
@@ -4,40 +4,10 @@ export default class Key {
   constructor(key) {
     this.keyItem = document.createElement('li');
     this.keyItem.className = 'key keyboard__item';
-    const keyCode = key.code;
-    const ctrkeys = [
-      'Backspace',
-      'Tab',
-      'Delete',
-      'CapsLock',
-      'Enter',
-      'ShiftLeft',
-      'ArrowUp',
-      'ShiftRight',
-      'ControlLeft',
-      'MetaLeft',
-      'AltLeft',
-      'Space',
-      'AltRight',
-      'ArrowLeft',
-      'ArrowDown',
-      'ArrowRight',
-      'ControlRight',
-    ];
-    if (keyCode.includes('Key')) {
-      this.keyItem.classList.add('key_letter');
-    } else if (ctrkeys.includes(keyCode)) {
-      const keyClass = keyCode.toLowerCase();
-      this.keyItem.classList.add(`key_${keyClass}`);
-    }
     const keyButton = document.createElement('button');
     keyButton.className = 'key__button';
-    const template = `
-      <span class="key__value-en-default key__value_active">${key.key.en.default}</span>
-      <span class="key__value-en-shift">${key.key.en.shift}</span>
-      <span class="key__value-ru-default">${key.key.ru.default}</span>
-      <span class="key__value-ru-shift">${key.key.ru.shift}</span>`;
-    keyButton.innerHTML = template;
+    keyButton.dataset.keyCode = key.code;
+    keyButton.innerText = key.key.en.default;
     this.keyItem.appendChild(keyButton);
   }
 
